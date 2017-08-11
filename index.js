1737662abdf8ec0ad8dcde23bc65beaaaf28d6e6
Kyle Billiot <kylelukej@gmail.com>
2017-08-11 08:41:22 -0600

    Added more code in index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require("express");
 var pug = require("pug");
+var cookieParser = require("cookieParser");
+var expressSession = require("expressSession");
 
 var app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/views"));
+app.use(expressSession({
+    secret: "t2w)R=9o%A;X&6;8#{DFM5,Sc00<8I1v*1OD2U9Tn``~rKdz-T!W17L'v61Z})Q(Xigi^r%CtHfi*f.l\"CjQqydtm22{l)KtJ6|b5QLzk_t-MD:~gN[tun<<vh9IOt",
+    saveUninitialized: true,
+    resave: true
+}));
 
 app.get("/", function(req, res) {
     res.render("index", {
