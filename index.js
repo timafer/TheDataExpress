e4c93c78432802d0abfdfadfd89b3fa250bc2ae6
Kyle Billiot <kylelukej@gmail.com>
2017-08-11 08:26:49 -0600

    Added pug routing.:

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ var pug = require("pug");
 
 var app = express();
 
-app.get("/", function(req, res) {res.send("It's up and running.")});
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/views"));
+
+app.get("/", function(req, res) {
+    res.render("index", {
+        title: "Homepage"
+    });
+});
+
+app.get("/:page", function(req, res) {
+    res.render(req.params.page, {
+        title: req.params.page
+    });
+});
 
 app.listen(3000);
