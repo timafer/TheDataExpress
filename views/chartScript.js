6249aa8ae7520418a9966e6f42c5b01e27876a36
D-Blake <dblake.cccmc@gmail.com>
2017-08-20 15:27:03 -0600

    Charts

---
+++ views/chartScript.js
@@ -0,0 +1,182 @@
+var canvas = document.getElementById('myCanvas1');
+var context = canvas.getContext('2d');
+
+//Answered A
+var aHeight = 100;
+var aX = 10;
+context.beginPath();
+context.rect(aX, 50, 50, aHeight);
+context.fillStyle = 'green';
+context.fill();
+context.lineWidth = 2;
+context.strokeStyle = 'black';
+context.stroke();
+context.fillStyle = 'black';
+context.font = '30pt Calibri';
+context.fillText('A', aX+10, aHeight-60);
+
+//Answered B
+var bHeight = 100;
+var bX = 70;
+context.beginPath();
+context.rect(bX, 50, 50, 100);
+context.fillStyle = 'yellow';
+context.fill();
+context.lineWidth = 2;
+context.strokeStyle = 'black';
+context.stroke();
+context.fillStyle = 'black';
+context.font = '30pt Calibri';
+context.fillText('B', bX+10, bHeight-60);
+
+//Answered C
+var cHeight = 100;
+var cX = 130;
+context.beginPath();
+context.rect(cX, 50, 50, 100);
+context.fillStyle = 'red';
+context.fill();
+context.lineWidth = 2;
+context.strokeStyle = 'black';
+context.stroke();
+context.fillStyle = 'black';
+context.font = '30pt Calibri';
+context.fillText('C', cX+10, cHeight-60);
+
+//Answered D
+var dHeight = 100;
+var dX = 190;
+context.beginPath();
+context.rect(dX, 50, 50, 100);
+context.fillStyle = 'blue';
+context.fill();
+context.lineWidth = 2;
+context.strokeStyle = 'black';
+context.stroke();
+context.fillStyle = 'black';
+context.font = '30pt Calibri';
+context.fillText('D', dX+10, dHeight-60);
+
+var canvas = document.getElementById('myCanvas2');
+var context = canvas.getContext('2d');
+
+//Answered A
+var aHeight = 100;
+var aX = 10;
+context.beginPath();
+context.rect(aX, 50, 50, aHeight);
+context.fillStyle = 'green';
+context.fill();
+context.lineWidth = 2;
+context.strokeStyle = 'black';
+context.stroke();
+context.fillStyle = 'black';
+context.font = '30pt Calibri';
+context.fillText('A', aX+10, aHeight-60);
+
+//Answered B
+var bHeight = 100;
+var bX = 70;
+context.beginPath();
+context.rect(bX, 50, 50, 100);
+context.fillStyle = 'yellow';
+context.fill();
+context.lineWidth = 2;
+context.strokeStyle = 'black';
+context.stroke();
+context.fillStyle = 'black';
+context.font = '30pt Calibri';
+context.fillText('B', bX+10, bHeight-60);
+
+//Answered C
+var cHeight = 100;
+var cX = 130;
+context.beginPath();
+context.rect(cX, 50, 50, 100);
+context.fillStyle = 'red';
+context.fill();
+context.lineWidth = 2;
+context.strokeStyle = 'black';
+context.stroke();
+context.fillStyle = 'black';
+context.font = '30pt Calibri';
+context.fillText('C', cX+10, cHeight-60);
+
+//Answered D
+var dHeight = 100;
+var dX = 190;
+context.beginPath();
+context.rect(dX, 50, 50, 100);
+context.fillStyle = 'blue';
+context.fill();
+context.lineWidth = 2;
+context.strokeStyle = 'black';
+context.stroke();
+context.fillStyle = 'black';
+context.font = '30pt Calibri';
+context.fillText('D', dX+10, dHeight-60);
+
+var canvas = document.getElementById('myCanvas3');
+var context = canvas.getContext('2d');
+
+//Answered A
+var aHeight = 100;
+var aX = 10;
+context.beginPath();
+context.rect(aX, 50, 50, aHeight);
+context.fillStyle = 'green';
+context.fill();
+context.lineWidth = 2;
+context.strokeStyle = 'black';
+context.stroke();
+context.fillStyle = 'black';
+context.font = '30pt Calibri';
+context.fillText('A', aX+10, aHeight-60);
+
+//Answered B
+var bHeight = 100;
+var bX = 70;
+context.beginPath();
+context.rect(bX, 50, 50, 100);
+context.fillStyle = 'yellow';
+context.fill();
+context.lineWidth = 2;
+context.strokeStyle = 'black';
+context.stroke();
+context.fillStyle = 'black';
+context.font = '30pt Calibri';
+context.fillText('B', bX+10, bHeight-60);
+
+//Answered C
+var cHeight = 100;
+var cX = 130;
+context.beginPath();
+context.rect(cX, 50, 50, 100);
+context.fillStyle = 'red';
+context.fill();
+context.lineWidth = 2;
+context.strokeStyle = 'black';
+context.stroke();
+context.fillStyle = 'black';
+context.font = '30pt Calibri';
+context.fillText('C', cX+10, cHeight-60);
+
+//Answered D
+var dHeight = 100;
+var dX = 190;
+context.beginPath();
+context.rect(dX, 50, 50, 100);
+context.fillStyle = 'blue';
+context.fill();
+context.lineWidth = 2;
+context.strokeStyle = 'black';
+context.stroke();
+context.fillStyle = 'black';
+context.font = '30pt Calibri';
+context.fillText('D', dX+10, dHeight-60);
+
+
+
+
+
+
